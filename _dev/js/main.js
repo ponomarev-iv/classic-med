72bd2b128b87d71eae10aa59aff99c8bbf4b5a85
Pompo86 <ilya86ponomarev@gmail.com>
2017-06-14 23:37:41 +0500

    Добавил favicon

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -68,9 +68,26 @@ function popupForm(){
     });
 }
 
+function popupGallery(){
+    $(document).ready(function() {
+        $('.popup-gallery').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            tLoading: 'Загрузка изборажения #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+            }
+        });
+    });
+}
+
 
 $(document).ready(function(){
     swiperInit();
     mobileMenu();
     popupForm();
+    popupGallery();
 });
